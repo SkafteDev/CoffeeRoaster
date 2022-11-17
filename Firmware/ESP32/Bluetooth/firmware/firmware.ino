1d64d1d8723f323c92f1c0bfb2aeb92fc1cadf96
Christian Clausen <csbc@mmmi.sdu.dk>
2022-11-17 01:34:01 +0100

    Added bluetooth msg dispatch for sensing and control.

--- Firmware/ESP32/Bluetooth/firmware/firmware.ino
+++ Firmware/ESP32/Bluetooth/firmware/firmware.ino
@@ -25,7 +25,7 @@ int thermoCLK = 5;
 // Duty cycle of 100% means that the heater is on at all times.
 // Duty cycle of 25% means that the heater is on for 250 [ms] of a second and off 750 [ms] of a second.
 // Duty cycle of 75% means that the heater is on for 750 [ms] of a second and off 250 [ms] of a second.
-float dutyCycle = 0;
+float _dutyCycle = 0;
 
 // The current temperature of the heat chamber.
 float currentTemp = 0;
@@ -35,6 +35,9 @@ float currentTemp = 0;
 
 // Thread that handles the inbound bluetooth serial data.
 TaskHandle_t inboundBluetooth_t;
+
+// Thread that handles the dispatch of inbound bluetooth serial data.
+TaskHandle_t dispatchInboundBluetooth_t;
 
 void setup() {
   Serial.begin(115200);
@@ -59,6 +62,15 @@ void setup() {
       NULL,  /* Task input parameter */
       1,  /* Priority of the task */
       &inboundBluetooth_t,  /* Task handle. */
+      0); /* Core where the task should run */
+
+ xTaskCreatePinnedToCore(
+      dispatchInboundBT, /* Function to implement the task */
+      "DispatchInboundBT", /* Name of the task */
+      10000,  /* Stack size in words */
+      NULL,  /* Task input parameter */
+      1,  /* Priority of the task */
+      &dispatchInboundBluetooth_t,  /* Task handle. */
       0); /* Core where the task should run */
 
   delay(1000);
@@ -100,9 +112,9 @@ void readTemp(void * pvParameters) {
  * Example...
  * [W001VALUE] translates to WRITE identifier 001 with the payload VALUE.
  */
-int bufferSize = 256;
-char buffer[256]; // Buffer to store the incoming packet.
-bool bufferReady = false; // Flag to determine if the buffer is ready to be read.
+int _bufferSize = 256;
+std::vector<char> _buffer; // Buffer to store the incoming packet.
+bool _bufferReady = false; // Flag to determine if the buffer is ready to be read.
 void readInboundBT(void * parameters) {  
   for (;;) {
     while (!SerialBT.hasClient()) {
@@ -110,15 +122,16 @@ void readInboundBT(void * parameters) {
       delay(250);
     }
     
-    if (bufferReady == true) {
+    if (_bufferReady == true) {
       Serial.println("readInboundBT(): Buffer ready to be read.");
       delay(250);
       continue;
     }
       
-    for (int i = 0; i < bufferSize; i++) {      
+    for (int i = 0; i < _bufferSize; i++) {      
       if (!SerialBT.connected()) {
         Serial.println("readInboundBT(): Client disconnected. Flushing buffer.");
+        _buffer.clear();
         break;
       }
       while(SerialBT.connected() && !SerialBT.available()) {
@@ -128,31 +141,68 @@ void readInboundBT(void * parameters) {
       
       if (SerialBT.available()) {
         char incomingChar = SerialBT.read();
-        //Serial.println("readInboundBT(): Received '" + String(incomingChar) + "'");
         Serial.println("readInboundBT(): Received '" + String(byte(incomingChar)) + "'");
-        buffer[i] = incomingChar;
+        _buffer.push_back(incomingChar);
       }
 
       // Buffer is ready to be read if its full or carriage return (CR) line feed (LF) is detected.
-      bool crlf = byte(buffer[i-1]) == 13 && byte(buffer[i]) == 10;
-      if (crlf || i == bufferSize-1) {
-        bufferReady = true;
+      bool crlf = (i-1 == -1) ? false : byte(_buffer.at(i-1)) == 13 && byte(_buffer.at(i)) == 10;
+      if (crlf || i == _bufferSize-1) {
+        _bufferReady = true;
         break;
       } else {
-        bufferReady = false;
+        _bufferReady = false;
       }
     }
   }
 }
 
-void loop() {
-  dutyCycle = 0; // TODO: Get value from bluetooth
+void dispatchInboundBT(void * parameters) {
+  for(;;) {
+    while (!_bufferReady) {
+      delay(20);
+    }
+    
+    if (_bufferReady) {
+      String msg = parseBuffer(_buffer);
+      flushBuffer();
+  
+      if (msg.startsWith("R001temperature")) {
+        // Write the temperature to the BT.
+        String temperature = String(currentTemp);
+        for (int i = 0; i < temperature.length(); i++) {
+          SerialBT.write(temperature.charAt(i));
+        }
+        
+      } else if (msg.startsWith("W002dutycycle")) {
+        int index = 13; // Index where duty cycle value begins.
+        String dutyCycle = msg.substring(index, msg.length());
+        _dutyCycle = dutyCycle.toInt();
+        Serial.println(String(_dutyCycle));
+      }
+    }
+  }
+}
 
-  if (dutyCycle == 0) {
+String parseBuffer(std::vector<char> buffer) {
+  String out = "";
+  for (int i = 0; i < buffer.size(); i++) {
+    out = out + buffer.at(i);
+  }
+  return out;
+}
+
+void flushBuffer() {
+  _buffer.clear();
+  _bufferReady = false;
+}
+
+void loop() {
+  if (_dutyCycle == 0) {
     return; // Nothing to do. The heater must be off.
   }
 
-  float delayMs = dutyCycle * 10; // Transform the duty cyle [%] to the corresponding delay in [ms]. DelayMs is in the range [0, 1000] [ms].  
+  float delayMs = _dutyCycle * 10; // Transform the duty cyle [%] to the corresponding delay in [ms]. DelayMs is in the range [0, 1000] [ms].  
   
   digitalWrite(relayPin, HIGH); // Turn on on heater
   delay(delayMs); // Sleep for the fraction of the second while the heater is in ON state.
