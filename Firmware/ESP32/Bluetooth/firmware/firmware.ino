8a7d4ef44811f968eca56743c2c92af4ff38475a
Christian Clausen <csbc@mmmi.sdu.dk>
2022-12-02 01:13:59 +0100

    Implemented BLE instead of BT classic.

--- Firmware/ESP32/Bluetooth/firmware/firmware.ino
+++ Firmware/ESP32/Bluetooth/firmware/firmware.ino
@@ -1,17 +1,8 @@
 #include "max6675.h"
-#include "BluetoothSerial.h"
-#include "Roaster.pb.h"
-#include "nanopb/pb_common.h"
-#include "nanopb/pb.h"
-#include "nanopb/pb_encode.h"
-#include "nanopb/pb_decode.h"
-#include "cobs.c"
-// Check if bluetooth configs are enabled.
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
-BluetoothSerial SerialBT;
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
 
 // System boot time.
 float bootTime = 0;
@@ -39,15 +30,41 @@ float _currentTemp = 0;
 // Thread that handles the temperature reading.
 TaskHandle_t temperatureSensor_t;
 
-// Thread that handles the inbound bluetooth serial data.
-TaskHandle_t inboundBluetooth_t;
+// Thread that handles the BLE notify/read/write.
+TaskHandle_t bleTask_t;
 
-// Thread that handles the dispatch of inbound bluetooth serial data.
-TaskHandle_t dispatchInboundBluetooth_t;
+//BLE server name
+#define bleServerName "COFFEE_ROASTER_ESP32"
+// BLE SERVICE UUID
+#define SERVICE_UUID "91bad492-b950-4226-aa2b-4ede9fa42f59"
+BLECharacteristic temperatureCelsiusCharacteristics("cba1d466-344c-4be3-ab3f-189f80dd7518", BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor temperatureCelsiusDescriptor(BLEUUID((uint16_t)0x2902));
+
+BLECharacteristic dutyCycleCharacteristics("ca73b3ba-39f6-4ab3-91ae-186dc9577d99",
+                                              BLECharacteristic::PROPERTY_NOTIFY|
+                                              BLECharacteristic::PROPERTY_WRITE|
+                                              BLECharacteristic::PROPERTY_READ);
+BLEDescriptor dutyCycleDescriptor(BLEUUID((uint16_t)0x2903));
+
+BLEServer *pServer;
+bool deviceConnected = false;
+
+//Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+    Serial.println("BLE client connected.");
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    Serial.println("BLE client disconnected.");
+    pServer->getAdvertising()->start();
+    Serial.println("MyServerCallbacks::onDisconnect(): Waiting for a client connection to notify...");
+  }
+};
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("CoffeeRoaster"); //Bluetooth device name
   
   pinMode(relayPin, OUTPUT);
   bootTime = millis();
@@ -61,26 +78,48 @@ void setup() {
       &temperatureSensor_t,  /* Task handle. */
       0); /* Core where the task should run */
 
- xTaskCreatePinnedToCore(
-      readInboundBT, /* Function to implement the task */
-      "ReadInboundBT", /* Name of the task */
+  xTaskCreatePinnedToCore(
+      bleHandler, /* Function to implement the task */
+      "BLE_Handler", /* Name of the task */
       10000,  /* Stack size in words */
       NULL,  /* Task input parameter */
-      1,  /* Priority of the task */
-      &inboundBluetooth_t,  /* Task handle. */
-      0); /* Core where the task should run */
-
- xTaskCreatePinnedToCore(
-      dispatchInboundBT, /* Function to implement the task */
-      "DispatchInboundBT", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      1,  /* Priority of the task */
-      &dispatchInboundBluetooth_t,  /* Task handle. */
+      0,  /* Priority of the task */
+      &bleTask_t,  /* Task handle. */
       0); /* Core where the task should run */
 
   delay(1000);
   Serial.println("setup() completed.");
+}
+
+void setupBLE() {
+  // Create the BLE Device
+  BLEDevice::init(bleServerName);
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *bleService = pServer->createService(SERVICE_UUID);
+
+  // Temperature
+  bleService->addCharacteristic(&temperatureCelsiusCharacteristics);
+  temperatureCelsiusDescriptor.setValue("Temperature Celsius");
+  temperatureCelsiusCharacteristics.addDescriptor(&temperatureCelsiusDescriptor); 
+
+  // Duty cycle
+  bleService->addCharacteristic(&dutyCycleCharacteristics);
+  dutyCycleDescriptor.setValue("Duty cycle");
+  dutyCycleCharacteristics.addDescriptor(&dutyCycleDescriptor);
+  
+  // Start the service
+  bleService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pServer->getAdvertising()->start();
+  Serial.println("setupBLE(): Waiting for a client connection to notify...");
 }
 
 /**
@@ -99,139 +138,29 @@ void readTemp(void * pvParameters) {
   }
 }
 
-/*
- * PROTOCOL:
- * Data are read from the serial bluetooth channel as a stream of bytes.
- * Packets can have max length of 256 bytes.
- * Packets are encoded with COBS (Consistent Overhead Byte Stuffing).
- * Packets are delimited by 00.
- *
-*/
-int _bufferSize = 256;
-uint8_t _packetDelimiter = 0;
-std::vector<uint8_t> _buffer; // Buffer to store the incoming packet.
-bool _bufferReady = false; // Flag to determine if the buffer is ready to be read.
-void readInboundBT(void * parameters) {
-  for (;;) {
-    while (!SerialBT.hasClient()) {
-      Serial.println("readInboundBT(): Waiting for BT client to connect.");  
-      delay(250);
-    }
+void bleHandler(void * pvParameters) {
+  setupBLE();
+
+  while(true) {
+    if (deviceConnected) {
+      int currentTemp = _currentTemp; // Only ints allowed.
+      temperatureCelsiusCharacteristics.setValue(currentTemp);
+      temperatureCelsiusCharacteristics.notify();
     
-    if (_bufferReady == true) {
-      Serial.println("readInboundBT(): Buffer ready to be read.");
-      delay(250);
-      continue;
-    }
-
-    while (_buffer.size() < _bufferSize && !checkPacketDelimiter(_buffer, _packetDelimiter)) {      
-      if (!SerialBT.connected()) {
-        Serial.println("readInboundBT(): Client disconnected. Flushing buffer.");
-        _buffer.clear();
-        break;
+      std::string newDutyCycleValStr = dutyCycleCharacteristics.getValue();
+      int newDutyCycleVal = atoi(newDutyCycleValStr.c_str());
+      if (0 <= newDutyCycleVal && newDutyCycleVal <= 100) { // Duty cycle must be between [0, 100].
+        _dutyCycle = newDutyCycleVal;
+        dutyCycleCharacteristics.notify();
       }
-      while(SerialBT.connected() && !SerialBT.available()) {
-        Serial.println("readInboundBT(): Waiting for inbound data.");
-        delay(250); // Wait for for inbound data.
-      }
-      
-      if (SerialBT.available()) {
-        uint8_t incomingChar = SerialBT.read();
-        Serial.print("readInboundBT(): Received ");
-        Serial.println(incomingChar, HEX);
-        _buffer.push_back(incomingChar);
-      }
+  
+      Serial.print("bleHandler(): ");
+      Serial.print("Notified (temperature, " + String(currentTemp) + "), (dutyCycle, " + String(_dutyCycle) + ")");
+      Serial.println();
     }
 
-    // Discard packet if packet size is too large and not delimited.
-    if (_buffer.size() == _bufferSize && !checkPacketDelimiter(_buffer, _packetDelimiter)) {
-      _buffer.clear(); // Discard packet.
-      _bufferReady = false;
-      Serial.println("readInboundBT(): Packet invalid. Discarded.");
-    } else {
-      _bufferReady = true; // Buffer is valid.
-      Serial.println("readInboundBT(): Packet ready for dispatch.");
-    }
+    delay(1000);
   }
-}
-
-bool checkPacketDelimiter(std::vector<uint8_t> buffer, uint8_t delimiter) {
-  return buffer.size() < 1 ? false : _buffer.at(buffer.size()-1) == 0;
-}
-
-void dispatchInboundBT(void * parameters) {
-  for(;;) {
-    while (!_bufferReady) {
-      delay(20);
-    }
-    
-    if (_bufferReady) {
-      // Apply COBS decoding on the packet buffer.
-      uint8_t encoded[_buffer.size()];
-      uint8_t decoded[sizeof(encoded)-2]; // Decoded COBS packet is 2 bytes shorter than the encoded COBS message.
-      convertToArr(_buffer, encoded); // Convert the packet buffer to a static array.
-      cobsDecode(encoded, _buffer.size(), decoded); // COBS decoding.
-      flushBuffer(); // Flush packet buffer.
-      
-      // TODO: Implement protobuffer decoding.
-      pb_istream_t istream;
-      istream = pb_istream_from_buffer(decoded, sizeof(decoded));
-      SensorResponse decoded_proto = SensorResponse_init_zero;
-      pb_decode(&istream, SensorResponse_fields, &decoded_proto);
-      Serial.println("Decoded protobuffer message:");
-      Serial.println(decoded_proto.sensor_id);
-      Serial.println(decoded_proto.sensor_value);
-
-      String msg = "";
-      if (msg.startsWith("R001temperature")) {
-        // Write the temperature to the BT.
-        String temperature = String(_currentTemp);
-        sendStringBT(temperature);
-        
-      } else if (msg.startsWith("W002dutycycle")) {
-        int index = 13; // Index where duty cycle value begins.
-        String dutyCycle = msg.substring(index, msg.length());
-        _dutyCycle = dutyCycle.toInt();
-        String response = String(_dutyCycle);
-        sendStringBT(response);
-        Serial.println("Setting duty cycle: " + String(_dutyCycle));
-      }
-    }
-  }
-}
-
-void convertToArr(std::vector<uint8_t> vec, uint8_t *out) {  
-  for (int i = 0; i < vec.size(); i++) {
-    out[i] = vec.at(i);
-  }
-}
-
-void sendStringBT(String str) {
-  for (int i = 0; i < str.length(); i++) {
-    SerialBT.write(str.charAt(i));
-  }
-}
-
-String parseBuffer(std::vector<uint8_t> buffer) {
-  String out = "";
-  for (int i = 0; i < buffer.size(); i++) {
-    out = out + buffer.at(i);
-  }
-  return out;
-}
-
-String parseBuffer2(uint8_t *buffer, int size) {
-  String out = "";
-  for (int i = 0; i < size; i++) {
-    out = out + "|" + buffer[i];
-    Serial.println(buffer[i], HEX);
-  }
-  return out;
-}
-
-void flushBuffer() {
-  _buffer.clear();
-  _bufferReady = false;
 }
 
 void loop() {
--- protobuffer/README.txt
+++
@@ -1,7 +0,0 @@
-
-Steps to generate .h and .c files:
-1) Download nanopb files.
-2) cd into nanopb/generator
-3) Execute the following command
-
-./nanopb_generator.py -I PROTO_FILE_DIR -D OUTPUT_DIR -f PROTO_OPTIONS_FILE PROTO_FILE
